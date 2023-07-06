0ee1e153e868e908ad75b0da001a995d9a41899c
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-07-06 23:41:33 +0900

    Add files via upload

---
+++ 백준/GuGuDan.java
@@ -0,0 +1,20 @@
+
+/*
+* 백준 2739번 구구단
+*
+* */
+import java.util.Scanner;
+public class GuGuDan {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a;
+
+            a = sc.nextInt();
+                for(int j=1; j<10; j++){
+                    System.out.println(a + " * " + j + " = " +(a*j));
+                }
+            }
+        }
+
+
+
