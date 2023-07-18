01b0676c1060722f45a7566c76c9e431645b15b8
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-07-18 22:48:41 +0900

    Add files via upload

---
+++ 백준/AB_4.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+
+public class AB_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+        while(sc.hasNext()){
+            a = sc.nextInt();
+            b = sc.nextInt();
+
+            System.out.println(a+b);
+
+        }
+    }
+}
