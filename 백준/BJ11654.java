9f633adea1731554bf3a81bdf8f0f4534ecc4ddb
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-07-30 18:35:33 +0900

    Add files via upload

---
+++ 백준/BJ11654.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.util.Scanner;
+
+public class BJ11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch;
+
+        ch = sc.nextLine().charAt(0);
+        System.out.println((int)ch);
+    }
+}
