08ea48bf1c633a82e9c0843006e50e2c9caadd7e
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-08-01 14:06:32 +0900

    Add files via upload

---
+++ 백준/BJ11718.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.Scanner;
+
+public class BJ11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+    }
+}
