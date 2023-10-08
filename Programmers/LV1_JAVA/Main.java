fc90cd89e75210e614bcbdf5c290e704966b6ad9
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-10-08 17:19:46 +0900

    Add files via upload

---
+++ Programmers/LV1_JAVA/Main.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String str : participant){
+            hm.put(str, hm.getOrDefault(str,0)+1);
+        }
+        for(String str : completion){
+            hm.put(str, hm.get(str)-1);
+        }
+        for(String str : hm.keySet()){
+            if(hm.get(str) != 0){
+                answer = str;
+                break;
+            }
+        }
+        return answer;
+    }
+}
