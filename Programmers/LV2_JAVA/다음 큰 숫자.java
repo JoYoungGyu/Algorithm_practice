446aac3e274924b9f44e08dbe05c0e950b2585cb
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-10-15 14:49:59 +0900

    Update 다음 큰 숫자.java

--- Programmers/LV2_JAVA/다음 큰 숫자.java
+++ Programmers/LV2_JAVA/다음 큰 숫자.java
@@ -2,15 +2,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int n_Cnt = Integer.bitCount(n);
-        int next_Cnt = 0;
+        int index = 1;
+        String n_len = Integer.toBinaryString(n);
+        n_len = n_len.replaceAll("0","");
+        int next_N = n;
         while(true){
-            n = n+1;
-            next_Cnt = Integer.bitCount(n);
-            if(n_Cnt == next_Cnt){
-                answer = n;
+            next_N = next_N + 1;
+            String next_N_Len = Integer.toBinaryString(next_N);
+            next_N_Len = next_N_Len.replaceAll("0","");
+            if(n_len.length() == next_N_Len.length()){
+                answer = next_N;
                 break;
             }
+            index++;
         }
         return answer;
     }
