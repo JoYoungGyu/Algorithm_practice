287eaf5ca16eb5349e36587166e57f076fd4ef1b
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-11-09 22:55:56 +0900

    Add files via upload

---
+++ 백준/Main.java
@@ -0,0 +1,28 @@
+package me.joyeonggyu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(x, w-x);
+        int min2 = Math.min(y, h-y);
+
+        bw.write(String.valueOf(Math.min(min,min2)));
+
+
+        bw.close();
+    }
+}
