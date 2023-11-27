875e3ec0394ab080e734207d5d3138cdcc21b0b7
JoYoungGyu <106457304+JoYoungGyu@users.noreply.github.com>
2023-11-27 23:22:49 +0900

    Add files via upload

---
+++ 백준/Main.java
@@ -0,0 +1,63 @@
+package me.joyeonggyu;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main{
+    public static int[] getYear(int year){
+        int[] month = {31 , 28 , 31 , 30 , 31 , 30 , 31 , 31 , 30 , 31 , 30 , 31};
+        if(year%4==0 && year%100 != 0 || year%400==0){
+            month[1] = 29;
+        }
+        return month;
+    }
+
+    public static int getDays(int year, int month , int day){
+        int days = 0;
+        int[] months;
+        // 각 년도 계산
+        for(int i=1; i<year; i++){
+            months = getYear(i);
+            for(int j=0; j<12; j++){
+                days += months[j];
+            }
+        }
+        // 남은 월 계산
+        months = getYear(year);
+        for(int i=0; i< month-1; i++){
+            days += months[i];
+        }
+        days+=day;
+        return days;
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] Tdate = br.readLine().split(" ");
+        String[] Ddate = br.readLine().split(" ");
+
+        int Tyear = Integer.parseInt(Tdate[0]);
+        int Tmonth = Integer.parseInt(Tdate[1]);
+        int Tday = Integer.parseInt(Tdate[2]);
+
+        int Dyear = Integer.parseInt(Ddate[0]);
+        int Dmonth = Integer.parseInt(Ddate[1]);
+        int Dday = Integer.parseInt(Ddate[2]);
+
+        int Tdays = getDays(Tyear, Tmonth , Tday);
+        int Ddays = getDays(Dyear, Dmonth , Dday);
+
+        if( (Dyear - Tyear > 1000) || (Dyear - Tyear == 1000 && Dmonth - Tmonth > 0) || (Dyear - Tyear == 1000 && Dmonth - Tmonth == 0 && Dday - Tday >= 0) ){
+            bw.write("gg");
+        }else{
+            bw.write("D-" + (Ddays - Tdays));
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
